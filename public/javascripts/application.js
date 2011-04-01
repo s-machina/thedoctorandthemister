f9b1f5f2177e73aa99613909baf676b77de09979
s-m <seanmichael.lewis@gmail.com>
2011-04-01 09:14:40 -0700

    Good enough loading.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     $('#bg').load(function() {
-        $(this).fadeIn(1000, function() {
-            $('#content').fadeIn(2000, function() {
-                $('#title').fadeIn(3000, function() {
-                    $('#date').fadeIn(1000);
-                });
+        $(this).fadeIn(2000, function() {
+            $('#content, #title').fadeIn(2000, function() {
+                $('#date').fadeIn(1000);
             });
         });
     });
