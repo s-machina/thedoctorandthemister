1ad3ffd4d3287a28b41580d631060301665c4165
s-m <seanmichael.lewis@gmail.com>
2011-07-11 00:35:11 -0700

    A bunch of stuff.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,28 @@
+$.fn.activate = function(callback) {
+    if ($(this).is(':visible')) {
+      return;
+    }
+    $(".section:visible").fadeOut(1000);
+    $(this).fadeIn(2000, callback);
+}
+
 $(document).ready(function() {
     $('#bg').load(function() {
-        $(this).fadeIn(2000, function() {
-            $('#content, #title').fadeIn(2000, function() {
+        $(this).activate(function() {
+            $('#footer, #title').fadeIn(2000, function() {
                 $('#date').fadeIn(1000);
             });
         });
     });
-});
 
+    $('a.section-link').click(function(e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+            return;
+        }
+        var section = $(this).attr('href');
+        $('a.section-link.active').removeClass('active');
+        $(this).addClass('active');
+        $('#' + section).activate();
+    });
+});
